626fbc17e12688b73e1c7c1c1e76c2c8cd10552f
Vũ Thành An <superlegend27@gmail.com>
2023-02-22 18:18:01 +0700

    adding automation test

---
+++ test/auth-middleware.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+
+const authMiddleware = require('../middleware/is-auth');
+
+describe('Auth middleware', function() {
+  it('should throw an error if no authorization header is present', function () {
+    const req = {
+      get: function (headername) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Not authenticated');
+  });
+  
+  it('should throw an error if the authorization header is only one string', function () {
+    const req = {
+      get: function (headername) {
+        return 'xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  })
+
+  it('should yield a userId after decoding the token', function() {
+    const req = {
+      get: function (headername) {
+        return 'Bearer asdfgfsdgsadf';
+      },
+    };
+    authMiddleware(req, {}, () => {})
+    expect(req).to.have.property("userId");
+  })
+});
